c67b299773917ce3f937b8f794945a49dc31018a
Stan01 <143421342+Jos0101@users.noreply.github.com>
2024-09-15 12:04:14 -0600

    Update desafioPromesa.js

--- Practica4_PE/desafioPromesa.js
+++ Practica4_PE/desafioPromesa.js
@@ -1,22 +1,24 @@
 
-function pedidoComida(comida, bebida){
-       return new Promise((resolve,reject)=>{
-           
-        if(comida && bebida){
-            resolve("pedido de comida exitoso : " + comida + " y " + bebida)
-          }else{
-            reject("ha ocurrido un error con su pedido, eliga una comida y bebida")
-          }
-       }
-    )
-
-}
-
-pedidoComida("hamburguesa", "cocacola")
- .then((mensaje)=>{
-    console.log(mensaje,)
- })
- .catch((error)=>{
-    console.error(error)
- }
-)
+function realizarPedido(pedido) {
+    return new Promise((resolve, reject) => {
+      console.log('procesando su pedido de', pedido, "...");
+  
+      setTimeout(() => {
+  
+        if (pedido) {
+          resolve(`el pedido fue realizado correctamente`);
+        } else {
+          reject('Hubo un problema al realizar su pedido');
+        }
+      }, 3000);
+    });
+  }
+  
+  realizarPedido("Pollo asado")
+    .then((mensaje) => {
+      console.log(mensaje);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  
